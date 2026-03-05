a13cca5beea85b80bb712473340fbefb30d7aa7e
charlieapples <appleyardcharles@gmail.com>
2026-03-05 20:09:47 +0000

    Implement service worker with caching strategy

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,22 @@
-self.addEventListener("install", e => {
-console.log("App installed");
+const CACHE_NAME = 'formulab-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/service-worker.js'
+];
+
+self.addEventListener('install', (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (evt) => {
+  evt.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (evt) => {
+  evt.respondWith(caches.match(evt.request).then(resp => resp || fetch(evt.request)));
 });
